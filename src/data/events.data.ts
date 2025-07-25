8d5b43e8b0e1dc6cbf7a8b0b29f1ea1bbc85ba68
Vincent <vincent.granie@2018.icam.fr>
2025-07-25 15:34:12 +0200

    update query with market_id

--- src/data/events.data.ts
+++ src/data/events.data.ts
@@ -16,72 +16,89 @@ export async function getEventsByAccount(
 ): Promise<RawEvent[]> {
   try {
     if (!isAddress(account) || !isAddress(market)) {
-      throw new Error("Bad Request");
+      throw new Error("Invalid account or market address");
     }
+
+    const marketResult = await fastify.pg.query<{ id: string }>(
+      `
+      SELECT id
+      FROM events.usg_markets
+      WHERE LOWER(contract_address) = LOWER($1)
+      `,
+      [market]
+    );
+
+    if (marketResult.rows.length === 0) {
+      throw new Error(`No market found for contract_address: ${market}`);
+    }
+
+    const market_id = marketResult.rows[0].id;
 
     const { rows } = await fastify.pg.query<RawEvent>(
       `
-      SELECT 'market_borrow' AS label, '0' AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_borrow
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'borrow' AS label, '0' AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.borrow
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_deposit' AS label, staked_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_deposit
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'deposit' AS label, staked_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.deposit
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_zap_deposit' AS label, staked_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_zap_deposit
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'zap_deposit' AS label, staked_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.zap_deposit
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_deposit_and_borrow' AS label, staked_amount AS collat_amount, borrow_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_deposit_and_borrow
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'deposit_and_borrow' AS label, staked_amount AS collat_amount, borrow_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.deposit_and_borrow
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_zap_deposit_and_borrow' AS label, staked_amount AS collat_amount, borrow_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_zap_deposit_and_borrow
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'zap_deposit_and_borrow' AS label, staked_amount AS collat_amount, borrow_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.zap_deposit_and_borrow
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_withdraw' AS label, withdrawn_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_withdraw
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'withdraw' AS label, withdrawn_amount AS collat_amount, '0' AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.withdraw
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_repay' AS label, '0' AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_repay
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'repay' AS label, '0' AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.repay
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_repay_and_withdraw' AS label, withdrawn_amount AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_repay_and_withdraw
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'repay_and_withdraw' AS label, withdrawn_amount AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.repay_and_withdraw
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_zap_repay' AS label, '0' AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_zap_repay
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'zap_repay' AS label, '0' AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.zap_repay
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_zap_repay_and_withdraw' AS label, withdrawn_amount AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_zap_repay_and_withdraw
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'zap_repay_and_withdraw' AS label, withdrawn_amount AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.zap_repay_and_withdraw
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_leverage' AS label, staked_amount AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_leverage
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'leverage' AS label, staked_amount AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.leverage
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_zap_leverage' AS label, staked_amount AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_zap_leverage
+      SELECT 'zap_leverage' AS label, staked_amount AS collat_amount, borrowed_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.zap_leverage
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_liquidate' AS label, collateral_liquidated AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_liquidate
+      SELECT 'liquidate' AS label, collateral_liquidated AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.liquidate
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       UNION ALL
-      SELECT 'market_self_liquidate' AS label, collateral_liquidated AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
-      FROM market_self_liquidate
-      WHERE LOWER(account) = LOWER($1) AND LOWER(market) = LOWER($2)
+      SELECT 'self_liquidate' AS label, collateral_liquidated AS collat_amount, repaid_amount AS usg_amount, block_date::text AS date, tx_hash
+      FROM events.self_liquidate
+      WHERE LOWER(account) = LOWER($1) AND market_id = $2
       ORDER BY date DESC
       `,
-      [account, market]
+      [account, market_id]
     );
 
     if (rows.length === 0) {
       fastify.log.warn(
-        `No events found for account: ${account}, market: ${market}`
+        `No events found for account: ${account}, market: ${market} (market_id: ${market_id})`
       );
     }
 
