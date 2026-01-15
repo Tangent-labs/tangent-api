d80bde38cb17eaf5618e9ea95583bbd8ea2fde6a
Vincent <vincent.granie@2018.icam.fr>
2026-01-15 18:40:00 +0100

    fix deleteMany on predeposit users

--- src/data/predeposit.data.ts
+++ src/data/predeposit.data.ts
@@ -1,74 +1,71 @@
 import { PrismaClient } from "@prisma/client"
-import { AddressLike } from "ethers";
+import { AddressLike } from "ethers"
 
 export class PredepositRepository {
-    prismaClient: PrismaClient
+  prismaClient: PrismaClient
 
-    constructor(prisma: PrismaClient) {
-        this.prismaClient = prisma
-    }
+  constructor(prisma: PrismaClient) {
+    this.prismaClient = prisma
+  }
 
-    async getAccountedTotals() {
-        return await this.prismaClient.accounted_total.findMany({
-            select: {
-                usg_lp: {
-                    select: {
-                        lp_name: true
-                    }
-                },
-                cap_lp: true,
-                total_lp: true,
-            }
-        })
-    }
+  async getAccountedTotals() {
+    return await this.prismaClient.accounted_total.findMany({
+      select: {
+        usg_lp: {
+          select: {
+            lp_name: true,
+          },
+        },
+        cap_lp: true,
+        total_lp: true,
+      },
+    })
+  }
 
-    async getAccountedBalances(userAddress: AddressLike) {
-        return await this.prismaClient.accounted_balances.findMany({
-            select: {
-                balance_lp: true,
-                usg_lp: {
-                    select: {
-                        lp_name: true
-                    }
-                }
-            },
-            where: {
-                user_address: {
-                    equals: userAddress.toString()
-                }
-            }
-        })
+  async getAccountedBalances(userAddress: AddressLike) {
+    return await this.prismaClient.accounted_balances.findMany({
+      select: {
+        balance_lp: true,
+        usg_lp: {
+          select: {
+            lp_name: true,
+          },
+        },
+      },
+      where: {
+        user_address: {
+          equals: userAddress.toString(),
+        },
+      },
+    })
+  }
 
-    }
+  async getPredepositState() {
+    return await this.prismaClient.predeposit_state.findFirst()
+  }
 
-    async getPredepositState() {
-        return await this.prismaClient.predeposit_state.findFirst()
-    }
+  async getUserStatus(userAddress: string) {
+    return await this.prismaClient.predeposit_users.findFirst({
+      where: {
+        user_address: {
+          equals: userAddress,
+        },
+      },
+    })
+  }
 
-    async getUserStatus(userAddress: string) {
-        return await this.prismaClient.predeposit_users.findFirst({
-            where: {
-                user_address: {
-                    equals: userAddress
-                }
-            }
-        })
-
-    }
-
-    async storeSignature(entry: { user_address: string, signature: string, is_private: boolean }) {
-        await this.prismaClient.$transaction(async (tx) => {
-            // Delete the old line
-            await tx.predeposit_users.delete({
-                where: {
-                    user_address: entry.user_address
-                }
-            })
-            // Insert the new line
-            await tx.predeposit_users.create({
-                data: entry
-            })
-        })
-    }
-
+  async storeSignature(entry: { user_address: string; signature: string; is_private: boolean }) {
+    await this.prismaClient.$transaction(async (tx) => {
+      // Delete the old line
+      await tx.predeposit_users.deleteMany({
+        where: {
+          user_address: entry.user_address,
+        },
+      })
+      // Insert the new line
+      await tx.predeposit_users.create({
+        data: entry,
+      })
+    })
+  }
 }
